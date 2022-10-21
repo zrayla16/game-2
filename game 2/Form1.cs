abd42fb258e754b9723d0db55e5f701a7014e0d8
zraykeel737 <zraykeel737@SDS-L-DLFDF63>
2022-10-21 12:01:50 -0400

    Add project files.

---
+++ game 2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game_2
+{
+    public partial class Form1 : Form
+    {
+        double random = 50%;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (page = page 1);
+            {
+                page 2;
+            }
+            page 2;
+            {
+                page 99;
+            }
+            page 3;
+            {
+                page 4;
+            }
+            page 4;
+            {
+                page 6
+            }
+            page 5;
+            {
+                page 99
+            }
+            page 6;
+            {
+                random;
+            }
+            page 7;
+            {
+                page 99
+            }
+            page 99;
+
+            page 100;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            page 1;
+
+            page 2;
+
+            page 3;
+
+            page 4;
+
+            page 5;
+
+            page 6;
+
+            page 7;
+
+            page 99;
+
+            page 100;
+        }
+    }
+}
