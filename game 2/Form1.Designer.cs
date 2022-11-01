ef99ccf9a587d9787564445987ade598a3930ad0
zraykeel737 <zraykeel737@SDS-L-DLFDF63>
2022-11-01 10:31:53 -0400

    hi

--- game 2/Form1.Designer.cs
+++ game 2/Form1.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.storyLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 96);
+            this.button1.Location = new System.Drawing.Point(12, 143);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(196, 51);
             this.button1.TabIndex = 0;
@@ -45,7 +45,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(218, 96);
+            this.button2.Location = new System.Drawing.Point(378, 143);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(196, 51);
             this.button2.TabIndex = 1;
@@ -53,21 +53,21 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label1
+            // storyLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(196, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.storyLabel.AutoSize = true;
+            this.storyLabel.Location = new System.Drawing.Point(9, 9);
+            this.storyLabel.Name = "storyLabel";
+            this.storyLabel.Size = new System.Drawing.Size(35, 13);
+            this.storyLabel.TabIndex = 2;
+            this.storyLabel.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(426, 159);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(586, 206);
+            this.Controls.Add(this.storyLabel);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -81,7 +81,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label storyLabel;
     }
 }
 
--- game 2/Form1.cs
+++ game 2/Form1.cs
@@ -12,67 +12,250 @@ namespace game_2
 {
     public partial class Form1 : Form
     {
-        double random = 50%;
+        int page = 1;
+        double random;
 
         public Form1()
         {
             InitializeComponent();
+            storyLabel.Text = "you have tow doors door one you don't now were it gos and door tow leds to a bathroom";
+            button1.Text = "door one";
+            button2.Text = "door two";
         }
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
-            if (page = page 1);
+            if (page == 1)
             {
-                page 2;
+                page = 2;
             }
-            page 2;
+            else if (page == 2)
             {
-                page 99;
+                page = 99;
             }
-            page 3;
+            else if (page == 3)
             {
-                page 4;
+                page = 4;
             }
-            page 4;
+            else if (page == 4)
             {
-                page 6
+                page = 6;
             }
-            page 5;
+            else if (page == 5)
             {
-                page 99
+                page = 99;
             }
-            page 6;
+            else if (page == 6)
             {
-                random;
+                // 99 or 100
+                page = 99;
             }
-            page 7;
+            else if (page == 7)
             {
-                page 99
+                page = 99;
             }
-            page 99;
+            else if (page == 99)
+            {
+                page = 101;
+            }
+            else if (page == 100)
+            {
+                page = 101;
+            }
+            else if (page == 101)
+            {
+                page = 1;
+            }
+            else if (page == 102)
+            {
+                page = 900;
+            }
+            else if (page == 900)
+            {
+                storyLabel.Text = ":)";
+            }
 
-            page 100;
+            switch (page)
+            {
+                case 1:
+                    storyLabel.Text = "you have tow doors door one you don't now were it gos and door tow leds to a bathroom";
+                    button1.Text = "door one";
+                    button2.Text = "door two";
+                    break;
+                case 2:
+                    storyLabel.Text = "go to the end page";
+                    button1.Text = "end page";
+                    button2.Text = "end page";
+                    break;
+                case 3:
+                    storyLabel.Text = "do you stay in the bathroom or leave";
+                    button1.Text = "stay";
+                    button2.Text = "leave";
+                    break;
+                case 4:
+                    storyLabel.Text = "there is a wendow do you jump out or stay in the bathroom";
+                    button1.Text = "jump";
+                    button2.Text = "stay";
+                    break;
+                case 5:
+                    storyLabel.Text = "go to the end page";
+                    button1.Text = "end page";
+                    button2.Text = "end page";
+                    break;
+                case 6:
+                    storyLabel.Text = "you jump out";
+                    button1.Text = "end page";
+                    button2.Text = "and pag";
+                    break;
+                case 7:
+                    storyLabel.Text = "you stay and get killed";
+                    button1.Text = "end page";
+                    button2.Text = "end pag";
+                    break;
+                case 8:
+                    storyLabel.Text = "you jump out and die do to the fall";
+                    button1.Text = "end pag";
+                    button2.Text = "end pag";
+                    break;
+                case 99:
+                    storyLabel.Text = "your died";
+                    button1.Text = "start agen";
+                    button2.Text = "end of game";
+                    break;
+                case 100:
+                    storyLabel.Text = "you wane";
+                    button1.Text = "start agen";
+                    button2.Text = "end of game";
+                    break;
+                case 101:
+                    storyLabel.Text = "you are starting agen";
+                    button1.Text = "go to start";
+                    button2.Text = "go to start";
+                    break;
+                case 102:
+                    storyLabel.Text = "you leving ok go bey";
+                    button1.Text = "end game";
+                    button2.Text = "end game";
+                    break;
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            page 1;
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 2)
+            {
+                page = 99;
+            }
+            else if (page == 3)
+            {
+                page = 5;
+            }
+            else if (page == 4)
+            {
+                page = 7;
+            }
+            else if (page == 5)
+            {
+                page = 99;
+            }
+            else if (page == 6)
+            {
+                // 99 or 100
+                page = 100;
+            }
+            else if (page == 7)
+            {
+                page = 99;
+            }
+            else if (page == 99)
+            {
+                page = 102;
+            }
+            else if (page == 100)
+            {
+                page = 102;
+            }
+            else if (page == 101)
+            {
+                page = 1;
+            }
+            else if (page == 102)
+            {
+                page = 900;
+            }
+            else if (page == 900)
+            {
+                storyLabel.Text = ":)";
+            }
 
-            page 2;
-
-            page 3;
-
-            page 4;
-
-            page 5;
-
-            page 6;
-
-            page 7;
-
-            page 99;
-
-            page 100;
+            switch (page)
+            {
+                case 1:
+                    storyLabel.Text = "you have tow doors door one you don't now were it gos and door tow leds to a bathroom";
+                    button1.Text = "door one";
+                    button2.Text = "door two";
+                    break;
+                case 2:
+                    storyLabel.Text = "go to the end page";
+                    button1.Text = "end page";
+                    button2.Text = "end page";
+                    break;
+                case 3:
+                    storyLabel.Text = "do you stay in the bathroom or leave";
+                    button1.Text = "stay";
+                    button2.Text = "leave";
+                    break;
+                case 4:
+                    storyLabel.Text = "there is a wendow do you jump out or stay in the bathroom";
+                    button1.Text = "jump";
+                    button2.Text = "stay";
+                    break;
+                case 5:
+                    storyLabel.Text = "go to the end page";
+                    button1.Text = "end page";
+                    button2.Text = "end page";
+                    break;
+                case 6:
+                    storyLabel.Text = "you jump out";
+                    button1.Text = "end page";
+                    button2.Text = "and pag";
+                    break;
+                case 7:
+                    storyLabel.Text = "you stay and get killed";
+                    button1.Text = "end page";
+                    button2.Text = "end pag";
+                    break;
+                case 8:
+                    storyLabel.Text = "you jump out and die do to the fall";
+                    button1.Text = "end pag";
+                    button2.Text = "end pag";
+                    break;
+                case 99:
+                    storyLabel.Text = "your died";
+                    button1.Text = "start agen";
+                    button2.Text = "end of game";
+                    break;
+                case 100:
+                    storyLabel.Text = "you wane";
+                    button1.Text = "start agen";
+                    button2.Text = "end of game";
+                    break;
+                case 101:
+                    storyLabel.Text = "you are starting agen";
+                    button1.Text = "go to start";
+                    button2.Text = "go to start";
+                    break;
+                case 102:
+                    storyLabel.Text = "you leving ok go bey";
+                    button1.Text = "end game";
+                    button2.Text = "end game";
+                    break;
+            }
         }
+     
     }
 }
